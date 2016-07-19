bb0265fc40ec60a80848bf82700ddbcf765c2b2c
Soheil <Soheil@Soheils-MacBook-Pro.local>
2016-07-19 19:23:43 +0800

    Init

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Testing' do
+	plugin 'cocoapods-rome'
+	pod 'OneDriveSDK'
+end
+
---
+++ Pods/Target Support Files/Pods-Testing-OneDriveSDK/Pods-Testing-OneDriveSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Testing_OneDriveSDK : NSObject
+@end
+@implementation PodsDummy_Pods_Testing_OneDriveSDK
+@end
